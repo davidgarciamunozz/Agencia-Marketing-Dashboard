2cf266d412a1cd5c3a4af55d78189d4ac59123f5
davidgarciamunozz <garxia0710@gmail.com>
2024-07-01 18:44:12 -0500

    builded

---
+++ src/containers/errors/Error404.jsx
@@ -0,0 +1,12 @@
+import Layout from "hocs/layout/Layout";
+
+
+function Error404 () {
+    return (
+        <Layout>
+            Error404
+        </Layout>
+    )
+}
+
+export default Error404;
---
+++ src/containers/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Layout from "hocs/layout/Layout";
+import { connect } from "react-redux";
+
+function Home () {
+    return (
+        <Layout>
+            Home
+        </Layout>
+    )
+}
+
+
+const mapStateToProps = state => ({
+        
+    });
+
+export default connect (mapStateToProps,{
+
+}) (Home);
